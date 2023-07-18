aa378d92bde24bc8cfaecd8d3b83bfa9baddbe5b
esteban <esteban.semellier@gmail.com>
2023-07-18 16:44:55 +0200

    add singin method

---
+++ TrendyTravel/Models/Media.swift
@@ -0,0 +1,8 @@
+//
+//  Media.swift
+//  TrendyTravel
+//
+//  Created by Esteban SEMELLIER on 18/07/2023.
+//
+
+import Foundation
---
+++ TrendyTravel/Views/RootView.swift
@@ -0,0 +1,20 @@
+//
+//  Root.swift
+//  TrendyTravel
+//
+//  Created by Esteban SEMELLIER on 18/07/2023.
+//
+
+import SwiftUI
+
+struct Root: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Root_Previews: PreviewProvider {
+    static var previews: some View {
+        Root()
+    }
+}
